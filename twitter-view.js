0cd160c4faf49f8fb95d29dab7c32b3f050c3827
Atticus White <contact@atticuswhite.com>
2015-09-27 05:43:34 -0400

    tweet rotator

--- twitter-view.js
+++ twitter-view.js
@@ -1,6 +1,8 @@
 'use strict';
 var React = require('react-native'),
-    Styles = require('./styles.js');
+    Styles = require('./styles.js'),
+    TweenState = require('react-tween-state');
+
 var {
   StyleSheet,
   View,
@@ -8,12 +10,52 @@ var {
   Image
 } = React;
 
-var TwitterView = React.createClass({
-  getInitialState: function () {
-    return {tweets: []};
-  },
+var tweets = [{
+  text: '#Office365 calendar tip to help book your rooms. How to add #meetingroom calendars.',
+  user: '@Robinpowered'
+}, {
+  text: 'Congress does not regulate Wall Street; Wall Street regulates Congress. But the time has come to say enough is enough!',
+  user: '@BernieSanders'
+}, {
+  text: 'Spelling snafu? Press the up arrow (long press on mobile) to quickly edit your last message, and preserve your dignity. #SlackTips 😮👆',
+  user: '@SlackHQ'
+}];
 
+var TwitterView = React.createClass({
+  mixins: [TweenState.Mixin],
+  getInitialState: function () {
+    return {tweet: 0};
+  },
+  rotate: function () {
+    var next = this.state.tweet + 1;
+    console.log('state', this.state.tweet, next, tweets.length, tweets);
+    if (next === tweets.length) {
+      next = 0;
+    }
+    console.log('next tweet', next, tweets);
+    this.setState({tweet: next});
+    this.fade(1);
+  },
+  fade: function (fadeDirection) {
+    this.tweenState('opacity', {
+      easing: TweenState.easingTypes.linear,
+      duration: 1000,
+      endValue: fadeDirection,
+      onEnd: function () {
+        if (!fadeDirection) {
+          // fade out, rotate
+          this.rotate();
+        } else {
+          setTimeout(this.fade.bind(this, 0), 5000);
+        }
+      }.bind(this)
+    });
+  },
+  componentDidMount: function () {
+    setTimeout(this.fade.bind(this, 0), 1000);
+  },
   render: function () {
+    var tweet = tweets[this.state.tweet];
     return (
       <View style={styles.container}>
         <View style={styles.row}>
@@ -21,9 +63,9 @@ var TwitterView = React.createClass({
           <Image source={require('image!twitter')} style={styles.image} />
         </View>
 
-        <View style={styles.tweet}>
-          <Text style={styles.text}>#Office365 calendar tip to help book your rooms. How to add #meetingroom calendars.</Text>
-          <Text style={styles.text}>@robinpowered</Text>
+        <View style={[styles.tweet, {opacity: this.getTweeningValue('opacity')}]}>
+          <Text style={styles.text}>{tweet.text}</Text>
+          <Text style={styles.text}>{tweet.user}</Text>
         </View>
 
       </View>
@@ -52,7 +94,8 @@ var styles = StyleSheet.create({
   text: {
     color: '#fff',
     fontSize: Styles.fontSize.small,
-    textAlign: 'right'
+    textAlign: 'right',
+    opacity: 1
   }
 });
 
