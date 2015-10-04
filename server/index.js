5ad761c6ea3f0e2bd7eef45134977121721e9d49
Atticus White <contact@atticuswhite.com>
2015-10-04 16:22:21 -0400

    live weather updating

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var exec = require('child_process').exec;
+var Promise = require('bluebird');
+var request = Promise.promisify(require('request'));
+var Config = require('../env.js');
 var app = express();
 
 app.use(express.static('dist'));
@@ -7,6 +10,14 @@ app.use(express.static('dist'));
 app.get('/sha', function (req, res) {
   exec('git rev-parse HEAD', function (error, stdout) {
     res.json({sha: stdout.toString()});
+  });
+});
+
+app.get('/weather', function (req, res) {
+  var endpoint = 'https://api.forecast.io/forecast/' + Config.forecastio.access_token + '/';
+  endpoint += '43.918364,-78.683535';
+  request(endpoint).spread(function (response, body) {
+    res.json(JSON.parse(body));
   });
 });
 
--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,26 +1,57 @@
 'use strict';
-
+var INTERVAL = 60 * 15 * 1000; // 15 minutes
 var React = require('react'),
     moment = require('moment');
+
+function getWeather () {
+  return fetch('/weather').then(function (response) {
+    return response.json();
+  });
+}
+
+function getCelcius (fahrenheit) {
+  return parseInt((fahrenheit - 32) * (5/9))
+}
 
 module.exports = React.createClass({
   displayName: 'weather',
   getInitialState: function () {
-    return {};
+    return {weather: null};
   },
-  tick: function () {
-    this.setState({});
+  updateWeather: function () {
+    return getWeather().then(function (weather) {
+      this.setState({weather: weather});
+    }.bind(this));
   },
   componentDidMount: function () {
-    // this.interval = setInterval(this.tick, 1000);
+    this.updateWeather().then(function () {
+      this.interval = setInterval(this.updateWeather, INTERVAL);
+    }.bind(this));
   },
   componentDidUnmount: function () {
-    // clearInterval(this.interval);
+    clearInterval(this.interval);
   },
   render: function () {
+    var weather = this.state.weather;
+    if (!weather) {
+      return <div></div>;
+    }
     return (
       <div className="weather">
-        55F (12C)
+        <span className="temperature">
+          {parseInt(weather.currently.temperature)}
+          <span className="super">o</span>
+          F
+        </span>
+
+
+        <span className="temperature">
+          (
+          {getCelcius(weather.currently.temperature)}
+          <span className="super">o</span>
+          C
+          )
+        </span>
       </div>
     );
   }
