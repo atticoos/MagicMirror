2efa9e8432042126d1f253597322538f72f00d8d
Atticus White <contact@atticuswhite.com>
2015-10-04 04:21:02 -0400

    change build flow

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    reactify = require('reactify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify');
 
 gulp.task('less', function () {
   gulp.src('src/less/main.less')
@@ -7,4 +11,35 @@ gulp.task('less', function () {
   .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('build', ['less']);
+gulp.task('browserify', function () {
+  gulp.src('src/index.jsx')
+  .pipe(browserify({
+    transform: [reactify],
+    insertGlobals: true
+  }))
+  .pipe(rename('bundle.js'))
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('vendor', function () {
+  gulp.src([
+    'bower_components/promise-polyfill/Promise.js',
+    'bower_components/fetch/fetch.js'
+  ])
+  .pipe(concat('vendor.js'))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', function () {
+  gulp.src('src/index.html')
+  .pipe(gulp.dest('dist/'));
+})
+
+gulp.task('js', ['browserify', 'vendor']);
+gulp.task('build', ['js', 'less', 'html']);
+
+gulp.task('watch', ['build'], function () {
+  gulp.watch(['src/**/*.js', 'src/**/*.jsx'], ['browserify']);
+  gulp.watch('src/**/*.less', ['less']);
+  gulp.watch('src/index.html', ['html']);
+});
